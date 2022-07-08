2d1addf18c3f66eeeb9a039aaa32c21f3c8ca581
MUHAMMAD-HUSSAIN-WAQAR <108922448+MUHAMMAD-HUSSAIN-WAQAR@users.noreply.github.com>
2022-07-08 00:31:39 -0700

    Add files via upload

---
+++ CWK_3.java
@@ -0,0 +1,10 @@
+public class CWK_3 {
+    public static void main(String arg []) {
+     //   int [] m;
+     //   m=new int[3];
+      int  m []  = {1,2,4};
+        System.out.println(m[0]);
+        System.out.println(m[1]);
+        System.out.println(m[2]);
+    }
+}
