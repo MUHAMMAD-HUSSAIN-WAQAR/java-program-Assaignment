a4d24d37318a39cf6db3ab895f70983822ee4768
MUHAMMAD-HUSSAIN-WAQAR <108922448+MUHAMMAD-HUSSAIN-WAQAR@users.noreply.github.com>
2022-07-09 03:24:04 -0700

    Add files via upload

---
+++ MultipleInheritance .java
@@ -0,0 +1,28 @@
+interface I1 
+ { 
+          
+ } 
+ interface I2 
+ { 
+          
+ } 
+ interface I3 
+ { 
+          
+ } 
+ class A 
+ { 
+          
+ } 
+ class B extends A implements I1,I2 
+ { 
+          
+ } 
+  
+ public class MultipleInheritance 
+ { 
+         public static void main(String arg[]) 
+         { 
+                 System.out.print("Hello World"); 
+         } 
+ }
